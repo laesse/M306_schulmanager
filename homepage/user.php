d7f90901c932b5aa5bf9c89cae3d7cb5a6604fcb
Dominik <naef.dominik@hotmail.com>
2018-11-02 14:40:22 +0100

    include navigation.php at register

--- homepage/user.php
+++ homepage/user.php
@@ -184,55 +184,11 @@ function showRegistration() {
 	<body>
 		<!-- Uses a header that scrolls with the text, rather than staying locked at the top -->
 		<div class='mdl-layout mdl-js-layout'>
-  			<header class='mdl-layout__header mdl-layout__header--scroll'>
-    			<div class='mdl-layout__header-row'>
-      				<!-- Title -->
-      				<span class='mdl-layout-title'>Schulmanager</span>
-      				<!-- Add spacer, to align navigation to the right -->
-      				<div class='mdl-layout-spacer'></div>
-      					<!-- Navigation -->
-      					<nav class='mdl-navigation'>
-	";
-
-
-	if (isset($_SESSION['user'])) {
-		echo "
-        <a class='mdl-navigation__link' href='index.php'>Home</a>
-        <a class='mdl-navigation__link' href='note.php'>Note</a>
-		<a class='mdl-navigation__link' href='timetable.php'>Timetable</a>
-		<a class='mdl-navigation__link' href='index.php?status=logout'>Logout</a>
-		";
-	} else {
-		echo "
-		<a class='mdl-navigation__link' href='index.php'>Home</a>
-		";
-	}
-
+  	";
+	
+	include 'navigation.php';
+	
 	echo "
-	  					</nav>
-    				</div>
-  				</header>
-				<div class='mdl-layout__drawer'>
-    				<span class='mdl-layout-title'>Schulmanager</span>
-    				<nav class='mdl-navigation'>
-    ";
-
-	if (isset($_SESSION['user'])) {
-		echo "
-        <a class='mdl-navigation__link' href='index.php'>Home</a>
-        <a class='mdl-navigation__link' href='note.php'>Note</a>
-		<a class='mdl-navigation__link' href='timetable.php'>Timetable</a>
-		<a class='mdl-navigation__link' href='index.php?status=logout'>Logout</a>
-		";
-	} else {
-		echo "
-		<a class='mdl-navigation__link' href='index.php'>Home</a>
-		";
-	}
-
-	echo "
-					</nav>
-  				</div>
   				<main class='mdl-layout__content'>
     				<div class='page-content'>
 
