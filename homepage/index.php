66a3979ef77a12ecb9873d004ad232ba9ccefe20
Dominik <naef.dominik@hotmail.com>
2018-11-03 19:16:44 +0100

    delete footer temporary

--- homepage/index.php
+++ homepage/index.php
@@ -54,15 +54,6 @@ function showHome() {
 													
 					</div>
   				</main>
-			<footer class='mdl-mini-footer'>
-  				<div class='mdl-mini-footer__left-section'>
-    				<div class='mdl-logo'>TODO</div>
-    				<ul class='mdl-mini-footer__link-list'>
-      					<li><a href=''>Help</a></li>
-      					<li><a href=''>Privacy & Terms</a></li>
-    				</ul>
-  				</div>
-			</footer>
 			</div>
 		</body>
 	</html>
--- homepage/note.php
+++ homepage/note.php
@@ -239,15 +239,6 @@ function showNote() {
 
 	echo "
   </main>
-  <footer class='mdl-mini-footer'>
-  	<div class='mdl-mini-footer__left-section'>
-   		<div class='mdl-logo'>TODO</div>
-    		<ul class='mdl-mini-footer__link-list'>
-      			<li><a href=''>Help</a></li>
-      			<li><a href=''>Privacy & Terms</a></li>
-    		</ul>
-  	</div>
-  </footer>
 </div>
 </body>
 </html>
