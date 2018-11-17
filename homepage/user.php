652417106f34de2a2acd2157facd6f4007b0161c
Dominik <naef.dominik@hotmail.com>
2018-11-17 18:07:36 +0100

    delete footer from register

--- homepage/user.php
+++ homepage/user.php
@@ -240,15 +240,6 @@ function showRegistration() {
 
 					</div>
   				</main>
-				<footer class='mdl-mini-footer'>
-  				<div class='mdl-mini-footer__left-section'>
-    				<div class='mdl-logo'>TODO</div>
-    				<ul class='mdl-mini-footer__link-list'>
-      					<li><a href=''>Help</a></li>
-      					<li><a href=''>Privacy & Terms</a></li>
-    				</ul>
-  				</div>
-			</footer>
 			</div>
 		</body>
 	</html>
